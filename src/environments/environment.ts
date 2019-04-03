1ff45a0c4ed3afbac6ef3686d56976307a24bfc1
air <asilkhan-al@mail.ru>
2019-04-03 14:29:26 +0600

    Commit 3 April 2019

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://127.0.0.1:8000/api/',
-  url: 'http://127.0.0.1:8000/',
+  apiUrl: 'http://za.kaztap.kz/api/',
+  url: 'http://za.kaztap.kz/',
   production: false
 };
 
